5c58e6cde4958aef299b5edb89fb4db1549696b4
AbhishekSrivastav <abhisheksrivastav596@gmail.com>
2025-01-26 02:24:34 +0530

    fixed

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dishRoutes = require('./routes/dishRoutes');
 const userRoutes = require('./routes/userRoutes');
+const counterRoutes = require('./routes/counterRoutes');
 require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use('/dishes',dishRoutes);
 app.use('/users',userRoutes);
+app.use('/counters',counterRoutes);
 
 
 app.listen(PORT, () => {
