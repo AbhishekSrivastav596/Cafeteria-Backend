fda7b390306105088469c803fef378399fb65887
AbhishekSrivastav <abhisheksrivastav596@gmail.com>
2025-02-03 15:57:30 +0530

    added imageURL in counter schema

--- models/counter.js
+++ models/counter.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const counterSchema = new mongoose.Schema({
   name: { type: String, required: true },
   merchants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  // dishes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Dish' }] 
+  imageUrl : { type: String },
 });
 
 const Counter = mongoose.model('Counter', counterSchema);
