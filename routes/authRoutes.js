6fe793fac31d31e96b875e2cd993ae9f68eec44e
AbhishekSrivastav <abhisheksrivastav596@gmail.com>
2025-02-16 19:20:06 +0530

    including name and email also during token generation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -37,12 +37,26 @@ router.post('/login', async (req, res) => {
     }
 
     const token = jwt.sign(
-      { id: user._id, role: user.role },
+      {
+        id: user._id,
+        email: user.email,
+        role: user.role,
+        name: user.name,
+      },
       process.env.JWT_SECRET,
       { expiresIn: '3d' }
     );
 
-    res.status(200).json({ message: 'Login successful', token });
+    res.status(200).json({
+      message: 'Login successful',
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+      }
+    });
   } catch (err) {
     console.log(err)
     res.status(500).json({ message: 'Internal server error', error: err });
