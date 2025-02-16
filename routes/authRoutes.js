5e9cf41469703f03cc60556a1a0156da6092658d
AbhishekSrivastav <abhisheksrivastav596@gmail.com>
2025-02-16 14:43:49 +0530

    verify token and auth routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const User = require('../models/user');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+    const newUser = new User({ name, email, password, role });
+    console.log(newUser)
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: 'Internal server error', error: err });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(email)
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: '3d' }
+    );
+
+    res.status(200).json({ message: 'Login successful', token });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: 'Internal server error', error: err });
+  }
+});
+
+module.exports = router;
